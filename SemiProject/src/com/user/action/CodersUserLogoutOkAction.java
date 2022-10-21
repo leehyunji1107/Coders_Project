2fa2655b637bacf492231d27f0c6b2e28ad62c41
qjqtkwns <111169711+qjqtkwns@users.noreply.github.com>
2022-10-21 17:49:56 +0900

    20221021 - 3 카카오 , 네이버 , 페이스북 로그인 시스템 구현.
    이후 계획
    
    로그인 창 , 로그인 버튼 아이콘 디자인 작업

--- SemiProject/src/com/user/action/CodersUserLogoutOkAction.java
+++ SemiProject/src/com/user/action/CodersUserLogoutOkAction.java
@@ -18,37 +18,12 @@ public class CodersUserLogoutOkAction implements Action {
 		
 		ActionForward forward = new ActionForward();
 		
-		String token = (String)session.getAttribute("token");
-		
-		System.out.println("if 진입 전 토근 값 >>> " + token);
-		
-		// http://localhost:8282/SemiProject/user_login_ok.do?id=qjqtkwns1@naver.com&nickname=%EC%A0%95%EC%98%81%ED%9B%88&token=kakao
-		// http://localhost:8282/SemiProject/user_login_ok.do?id=wjddudgns9414@naver.com&nickname=%EC%A0%95%EC%98%81%ED%9B%88&token=naver
-		
-		if(token.equals("naver")) {
-			System.out.println("토근 값 >>> " + token);
+		session.invalidate();
 			
-			System.out.println("http://nid.naver.com/nidlogin.logout");
+		forward.setRedirect(false);
 			
-			session.invalidate();
+		forward.setPath("main.jsp");
 			
-			forward.setRedirect(false);
-			
-			forward.setPath("main.jsp");
-			
-		}else if(token.equals("kakao")){
-			System.out.println("토근 값 >>> " + token);
-			
-			System.out.println("메인으로 이동");
-			
-			session.invalidate();
-			
-			forward.setRedirect(false);
-			
-			forward.setPath("main.jsp");
-		
-		}
-		
 		return forward;
 	}
 
